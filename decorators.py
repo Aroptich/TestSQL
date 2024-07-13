98772b8c1e5f28baa8b8bd04fa0637877e8866b7
Alexsandr <artamoshkinas@gmail.com>
2024-07-13 11:18:13 +0700

    Добавлено: * Функции-декораторы вынесены в отдельный модуль

---
+++ decorators.py
@@ -0,0 +1,94 @@
+import pymysql
+from datetime import datetime
+from prettytable import PrettyTable
+
+from config import host, port, password, user, db_name
+
+def connect(func):
+    """Функция-декоратор для связи с БД"""
+
+    def wrapper(*args, **kwargs):
+        try:
+            connection = pymysql.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database=db_name,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Соединение с БД установлено")
+            try:
+                with connection.cursor() as cursor:
+                    if isinstance(func(*args, **kwargs), tuple):
+                        sql, *values = func(*args, **kwargs)
+                        if values is not None:
+                            cursor.execute(sql, values)
+                        else:
+                            cursor.execute(sql)
+                    else:
+                        cursor.execute(func(*args, **kwargs))
+                    connection.commit()
+            finally:
+                connection.close()
+
+        except Exception as err:
+            print("В соединение с БД отказано...")
+            print(err)
+
+    return wrapper
+
+
+def logger(func):
+    """Функция-декоратор для логирования команд БД"""
+
+    def wrapper(*args, **kwargs):
+        try:
+            with open('log.txt', 'a', encoding='utf-8') as file:
+                query = func(*args, **kwargs)
+                file.writelines(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\n")
+                file.writelines(f"{'=' * 35}\n")
+                file.writelines(f'{query}\n')
+                file.writelines(f"{'=' * 35}\n")
+                return query
+        except Exception as err:
+            print(err)
+
+    return wrapper
+
+
+def reading_data(func):
+    """Функция-декоратор для вывода данных из БД в консоль"""
+
+    def wrapper(*args, **kwargs):
+        try:
+            connection = pymysql.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                database=db_name,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Соединение с БД установлено")
+            try:
+                with connection.cursor() as cursor:
+                    res = cursor.execute(func(*args, **kwargs))
+                    if res <= 0:
+                        print(f'Данных не найдено!')
+                        return None
+                    x = PrettyTable()
+                    rows = cursor.fetchall()
+                    x.field_names = [row for row in rows[0]]
+                    for row in rows:
+                        x.add_row([row[i] for i in row])
+                    print(x)
+                    connection.commit()
+                    return res
+            finally:
+                connection.close()
+        except Exception as err:
+            print("В соединение с БД отказано...")
+            print(err)
+
+    return wrapper
--- main.py
+++ main.py
@@ -1,98 +1,6 @@
-from datetime import datetime
-from prettytable import PrettyTable
-
-import pymysql
 import hashlib
 
-from config import host, port, password, user, db_name
-
-
-def connect(func):
-    """Функция-декоратор для связи с БД"""
-
-    def wrapper(*args, **kwargs):
-        try:
-            connection = pymysql.connect(
-                host=host,
-                port=port,
-                user=user,
-                password=password,
-                database=db_name,
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("Соединение с БД установлено")
-            try:
-                with connection.cursor() as cursor:
-                    if isinstance(func(*args, **kwargs), tuple):
-                        sql, *values = func(*args, **kwargs)
-                        if values is not None:
-                            cursor.execute(sql, values)
-                        else:
-                            cursor.execute(sql)
-                    else:
-                        cursor.execute(func(*args, **kwargs))
-                    connection.commit()
-            finally:
-                connection.close()
-
-        except Exception as err:
-            print("В соединение с БД отказано...")
-            print(err)
-
-    return wrapper
-
-
-def logger(func):
-    """Функция-декоратор для логирования команд БД"""
-
-    def wrapper(*args, **kwargs):
-        try:
-            with open('log.txt', 'a', encoding='utf-8') as file:
-                query = func(*args, **kwargs)
-                file.writelines(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\n")
-                file.writelines(f"{'=' * 35}\n")
-                file.writelines(f'{query}\n')
-                file.writelines(f"{'=' * 35}\n")
-                return query
-        except Exception as err:
-            print(err)
-
-    return wrapper
-
-def reading_data(func):
-    """Функция-декоратор для вывода данных из БД в консоль"""
-    def wrapper(*args, **kwargs):
-        try:
-            connection = pymysql.connect(
-                host=host,
-                port=port,
-                user=user,
-                password=password,
-                database=db_name,
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            print("Соединение с БД установлено")
-            try:
-                with connection.cursor() as cursor:
-                    res = cursor.execute(func(*args, **kwargs))
-                    if res <= 0:
-                        print(f'Данных не найдено!')
-                        return None
-                    x = PrettyTable()
-                    rows = cursor.fetchall()
-                    x.field_names = [row for row in rows[0]]
-                    for row in rows:
-                        x.add_row([row[i] for i in row])
-                    print(x)
-                    connection.commit()
-                    return res
-            finally:
-                connection.close()
-        except Exception as err:
-            print("В соединение с БД отказано...")
-            print(err)
-
-    return wrapper
+from decorators import *
 
 
 @connect
@@ -100,10 +8,10 @@ def reading_data(func):
 def create_table(table_name) -> tuple:
     """Функция создает таблицу 'users' и возвращает строку """
     query = ("CREATE TABLE IF NOT EXISTS `%s`"
-            "(`id` int(11) NOT NULL AUTO_INCREMENT,"
-            "`email` varchar(100) UNIQUE NOT NULL ," 
-            "`password` varchar(255) NOT NULL,"
-            "PRIMARY KEY (`id`));")
+             "(`id` int(11) NOT NULL AUTO_INCREMENT,"
+             "`email` varchar(100) UNIQUE NOT NULL ,"
+             "`password` varchar(255) NOT NULL,"
+             "PRIMARY KEY (`id`));")
     return query, table_name
 
 
@@ -120,11 +28,11 @@ def list_users() -> str:
 def create_user(email: str, password: str) -> tuple:
     """Функция возвращает sql-запрос на создание нового пользователя в таблице `users"""
     query = ("INSERT INTO `'users'` (`email`, `password`) "
-            "VALUES (%s, %s);")
-    #Хеширование пароля
+             "VALUES (%s, %s);")
+    # Хеширование пароля
     hash_pass = hashlib.sha224(password.encode())
-    #Преобразуем объект хеширования в шестнадцатиричное число
-    safty_pass =hash_pass.hexdigest()
+    # Преобразуем объект хеширования в шестнадцатиричное число
+    safty_pass = hash_pass.hexdigest()
     return query, email, safty_pass
 
 
